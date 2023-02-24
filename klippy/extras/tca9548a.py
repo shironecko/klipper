6b8c6b395b4f5002eb55d0d6981a07eb27fea722
vii <vii@gamecraft.tech>
2023-02-24 23:16:22 +0200

    more logging

--- klippy/extras/tca9548a.py
+++ klippy/extras/tca9548a.py
@@ -15,8 +15,11 @@ class TCA9548A:
         self.i2c = bus.MCU_I2C_from_config(config, default_addr=TCA9548A_CHIP_ADDR)
 
         self.printer.add_object("tca9548a " + self.name, self)
+        self.ports_status = 0
 
     def set_port_enabled(self, port, enabled):
+        if enabled and self.ports_status != 0:
+            logging.error(f'tca9548a error: enabling more than one port {self.ports_status:0b}')
         port = min(port, 7)
         params = self.i2c.i2c_read([TCA9548A_CHIP_ADDR], 1)
         settings = params['response'][0]
@@ -25,6 +28,7 @@ class TCA9548A:
         else:
             settings &= ~(1 << port)
         
+        self.ports_status = settings
         self.i2c.i2c_write([TCA9548A_CHIP_ADDR, settings])
 
 def load_config_prefix(config):
